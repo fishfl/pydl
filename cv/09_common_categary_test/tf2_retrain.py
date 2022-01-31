a4ce28a7aee851619b0730fd16dee70e6906f960
fishfl <fishfl@163.com>
2022-01-31 08:16:02 +0800

    tfserving client

--- cv/09_common_categary_test/tf2_retrain.py
+++ cv/09_common_categary_test/tf2_retrain.py
@@ -1,3 +1,6 @@
+
+# python tf2_retrain.py --image_dir D:\dataset\porn_verify --output_graph D:\models\porn_verify\output_graph.pb --output_labels  D:\models\porn_verify\output_labels.txt --summaries_dir D:\models\porn_verify\retrain_logs --how_many_training_steps 5000 --model_dir D:\models\porn_verify\raw --bottleneck_dir D:\models\porn_verify\bottleneck
+
 # Copyright 2015 The TensorFlow Authors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
